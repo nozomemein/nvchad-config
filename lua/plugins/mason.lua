c7e93c4f66371d79312cb13c75d774d623aec1a6
Nozomi-Hijikata <nozomi.hijikata@techouse.jp>
2024-04-20 20:40:45 +0900

    fix: fix typo

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,11 +1,15 @@
 return {
   "williamboman/mason-lspconfig.nvim",
-  opts = {
-    ensure_installed = {
-      "lua-language-server",
-      "solargraph",
-      "typescript-language-server",
-      "gopls"
+  config = function()
+    require("mason-lspconfig").setup {
+      opts = {
+        ensure_installed = {
+          "lua_ls",
+          "solargraph",
+          "typescript-language-server",
+          "gopls"
+        }
+      }
     }
-  }
+  end,
 }
