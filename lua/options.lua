189e67be236ee3adbff28a55808908d38ff277be
Nozomi-Hijikata <nozomi.hijikata@techouse.jp>
2024-05-05 12:17:55 +0900

    feat: vimrcのoptionsをnvim側にも移植

--- lua/options.lua
+++ lua/options.lua
@@ -6,10 +6,25 @@ require "nvchad.options"
 -- o.cursorlineopt ='both' -- to enable cursorline!
 
 local o = vim.o
+
+-- general settings
+o.expandtab = true   -- Use spaces instead of tabs
+o.tabstop = 2        -- Number of spaces tabs count for
+o.shiftwidth = 2     -- Number of spaces to use for autoindent
+o.softtabstop = 2    -- Number of spaces a tab counts for in insert mode
+o.autoindent = true  -- Copy indent from current line when starting a new line
+o.smartindent = true -- Makes indenting smart
+o.number = true      -- Show line numbers
+o.hlsearch = true    -- Highlight search results
+o.ignorecase = true  -- Case insensitive searching
+o.laststatus = 2     -- Always display the status line
+o.incsearch = true   -- Incremental search
+
 -- nvim-ufo settings
 -- fold levelを99に設定しないと、ファイルを開いたときにfoldされてしまう
 o.foldlevel = 99
 o.foldlevelstart = 99
+
 
 local autocmd = vim.api.nvim_create_autocmd
 -- highlight yank contents
