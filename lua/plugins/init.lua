bd741c7cc2ce5ae22a96483a0909e4e94c69e2f6
Nozomi-Hijikata <nozomi.hijikata@techouse.jp>
2024-05-19 11:56:18 +0900

    feat: markdown previewの追加

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -106,5 +106,15 @@ return {
     config = function()
       require('treesj').setup()
     end,
-  }
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    lazy = false,
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  },
 }
