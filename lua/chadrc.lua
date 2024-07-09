6872ae0a45653236a7f6d9cc022791a027789016
Nozomi-Hijikata <nozomi.hijikata@techouse.jp>
2024-07-09 23:22:53 +0900

    fix color

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -5,20 +5,7 @@
 ---@type ChadrcConfig
 local M = {}
 
--- local highlights = require "highlights"
-local colors = {
-  blue = "#7aa2f7",
-  green = "#9ece6a",
-  red = "#f7768e",
-  orange = "#ff9e64",
-  yellow = "#e0af68",
-  purple = "#bb9af7",
-  cyan = "#7dcfff",
-  grey = "#565f89",
-  black = "#1a1b26",
-  white = "#c0caf5",
-  background = "#24283b",
-}
+local highlights = require "highlights"
 
 M.ui = {
   theme = "tokyonight",
@@ -32,53 +19,7 @@ M.ui = {
     overriden_modules = nil,
   },
 
-
-  hl_override = {
-    DiffAdd = {
-      fg = colors.green,
-      bg = colors.background,
-    },
-
-    DiffChange = {
-      fg = colors.blue,
-      bg = colors.background,
-    },
-
-    DiffDelete = {
-      fg = colors.red,
-      bg = colors.background,
-    },
-
-    DiffText = {
-      fg = colors.yellow,
-      bg = colors.background,
-    },
-
-    DiffAdded = {
-      fg = colors.green,
-      bg = colors.background,
-    },
-
-    DiffRemoved = {
-      fg = colors.red,
-      bg = colors.background,
-    },
-
-    DiffFile = {
-      fg = colors.blue,
-      bg = colors.background,
-    },
-
-    DiffNewFile = {
-      fg = colors.green,
-      bg = colors.background,
-    },
-
-    DiffOldFile = {
-      fg = colors.red,
-      bg = colors.background,
-    },
-  },
+  hl_override = highlights.hl_override,
 
   -- lazyload it when there are 1+ buffers
   tabufline = {
