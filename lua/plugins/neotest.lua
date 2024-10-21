5f1d864b6271a882352a93b21a92bef964cb9731
nozomemein <b8yukifsukeo999n@gmail.com>
2024-10-21 22:21:20 +0900

    add golang neotest adapter!

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -44,5 +44,22 @@ return {
         },
       })
     end
-  }
+  },
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      { "fredrikaverpil/neotest-golang", version = "*" }, -- Installation
+    },
+    config = function()
+      require("neotest").setup({
+        adapters = {
+          require("neotest-golang"), -- Registration
+        },
+      })
+    end,
+  },
 }
