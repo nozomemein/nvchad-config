6cd29b03b88ac7374421265bae82734e22ff1b04
Nozomi-Hijikata <nozomi.hijikata@techouse.jp>
2025-01-15 14:59:33 +0900

    style: change theme

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -7,7 +7,7 @@ local M = {}
 local highlights = require "highlights"
 
 M.base46 = {
-  theme = "chadracula",
+  theme = "poimandres",
   hl_override = highlights.hl_override,
 
   -- hl_override = {
